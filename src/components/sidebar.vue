b9022ae02a87a88fb0a474fb91ca919bf448ee0e
Amra09 <yoengamra9@gmail.com>
2023-11-17 10:21:45 +0700

    Modified sidebar

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -10,6 +10,7 @@
             <li><a href="">Home </a></li>
             <li><a href="">Home </a></li>
             <li><a href="">Home </a></li>
+            <li><a href="">Home </a></li>
         </ul>
     </div>
 </template>
